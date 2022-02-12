a172cd31a8584464c65f2b12419a9efd84a3730e
13.5coder <akifmansoori8287@gmail.com>
2022-02-12 12:05:57 +0530

    ChangingImages

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import profileImage from "../assets/profile----.png";
+import profileImage from "../assets/profile----.jpeg";
 export default function Testimonials() {
   return (
     <Section id="testimonials">
